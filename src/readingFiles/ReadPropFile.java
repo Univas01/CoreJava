d462ece1b5dbb3431342b46ce169d700943f8c27
Ola Ajala <ola.ajala@worldfirst.com>
2018-05-30 16:52:43 +0100

    refactoring readingFiles class

--- src/readingFiles/ReadPropFile.java
+++ src/readingFiles/ReadPropFile.java
@@ -1,8 +1,10 @@
 package readingFiles;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,33 +13,40 @@ import org.testng.annotations.Test;
 
 public class ReadPropFile {
 
-	WebDriver driver;
+	public static WebDriver driver;
+	public static Properties prop;
+
+	@BeforeMethod
+	public static void setUp() {
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
-	public void setUp() throws IOException {
-		
-		Properties prop = new Properties();
-		FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/config.properties");
-		prop.load(ip);
-
-		String userName = prop.getProperty("name");
-		String userAge = prop.getProperty("age");
-		String url = prop.getProperty("URL");
+	public static void initialization() {
 		String browserName = prop.getProperty("browser");
 
-		System.out.println(userName);
-		System.out.println(userAge);
-		System.out.println(url);
-		System.out.println(browserName);
-
-		if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/browsers/chromedriver");
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/browsers/chromedriver");
 			driver = new ChromeDriver();
-		} else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/browsers/geckodriver");
+		} else if (browserName.equalsIgnoreCase("FF")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/browsers/geckodriver");
 			driver = new FirefoxDriver();
 		}
-		driver.get(url);
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		driver.get(prop.getProperty("URL"));
 
 	}
 
