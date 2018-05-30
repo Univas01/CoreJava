0ac82c8b450ef813cc484eca802b4ba912fc40de
Ola Ajala <ola.ajala1@outlook.com>
2018-05-29 18:22:07 -0600

    user directory added

--- src/readingFiles/ReadPropFile.java
+++ src/readingFiles/ReadPropFile.java
@@ -9,14 +9,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import net.bytebuddy.utility.privilege.GetSystemPropertyAction;
+
 public class ReadPropFile {
 
 	WebDriver driver;
 
 	@Test
 	public void setUp() throws IOException {
+		
 		Properties prop = new Properties();
-		FileInputStream ip = new FileInputStream("/Users/ola.ajala/Desktop/OwnProject/CoreJava/config.properties");
+		FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/config.properties");
 		prop.load(ip);
 
 		String userName = prop.getProperty("name");
@@ -30,11 +33,10 @@ public class ReadPropFile {
 		System.out.println(browserName);
 
 		if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver",
-					"/Users/ola.ajala/Desktop/OwnProject/CoreJava/browsers/chromedriver");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/browsers/chromedriver");
 			driver = new ChromeDriver();
 		} else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/ola.ajala/Desktop/OwnProject/geckodriver");
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/browsers/geckodriver");
 			driver = new FirefoxDriver();
 		}
 		driver.get(url);
