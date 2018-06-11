3c14b69142ae6578aae7c285b86fb29b1889e743
Ola Ajala <ola.ajala@worldfirst.com>
2018-06-11 22:34:00 +0100

    update readpropfile class

--- src/readingFiles/ReadPropFile.java
+++ src/readingFiles/ReadPropFile.java
@@ -5,7 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
@@ -47,7 +50,40 @@ public class ReadPropFile {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 		driver.get(prop.getProperty("URL"));
-
+		
 	}
 
+	@Test(dependsOnMethods = {"initialization"})
+	public void loginTest() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys(prop.getProperty("userName"));
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(prop.getProperty("password"));
+		Thread.sleep(3000);
+		JavascriptExecutor webdriver = (JavascriptExecutor) driver;
+		webdriver.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='Login']")));
+		
+		/*WebElement crmpro = driver.findElement(By.xpath("//td[@class='logo_text']"));
+		if (crmpro.isDisplayed()) {
+			System.out.println("loginTest is a Pass");
+		}else{
+			System.out.println("loginTest is a Fail");
+		}*/
+		
+		System.out.println(driver.getTitle());
+		
+		driver.switchTo().frame(0);
+		
+		int size = driver.findElements(By.name("iframe")).size();
+		System.out.println("iframe size is " + size);
+		
+		//driver.findElement(By.xpath("//a[@href='https://www.freecrm.com/index.cfm?logout=1']")).click();
+		
+		Thread.sleep(3000);
+		webdriver.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[@href='https://www.freecrm.com/index.cfm?logout=1']")));
+		
+	}
+	
+	@Test(dependsOnMethods =  {"loginTest"})
+	public void tearDown() {
+		driver.quit();
+	}
 }
