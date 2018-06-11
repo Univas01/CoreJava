3eb6912aedce4017068b5aee337a2d9085455b5a
Ola Ajala <ola.ajala1@outlook.com>
2018-06-11 17:08:05 -0600

    adding webdriverwaitmethods

--- src/readingFiles/ReadPropFile.java
+++ src/readingFiles/ReadPropFile.java
@@ -14,10 +14,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import webdriverwaitmethods.WaitMethods;
+
 public class ReadPropFile {
 
 	public static WebDriver driver;
 	public static Properties prop;
+	public static WebElement userName, password, submit, logout;
 
 	@BeforeMethod
 	public static void setUp() {
@@ -46,40 +49,25 @@ public class ReadPropFile {
 
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("URL"));
-		
 	}
 
 	@Test(dependsOnMethods = {"initialization"})
-	public void loginTest() throws InterruptedException {
-		driver.findElement(By.xpath("//input[@name='username']")).sendKeys(prop.getProperty("userName"));
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(prop.getProperty("password"));
-		Thread.sleep(3000);
+	public void loginTest() {
+		userName = driver.findElement(By.xpath("//input[@name='username']"));
+		password = driver.findElement(By.xpath("//input[@name='password']"));
+		submit = driver.findElement(By.xpath("//input[@value='Login']"));
+
+		WaitMethods.sendKeys(driver, userName, 3, prop.getProperty("userName"));
+		WaitMethods.sendKeys(driver, password, 3, prop.getProperty("password"));
 		JavascriptExecutor webdriver = (JavascriptExecutor) driver;
-		webdriver.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='Login']")));
-		
-		/*WebElement crmpro = driver.findElement(By.xpath("//td[@class='logo_text']"));
-		if (crmpro.isDisplayed()) {
-			System.out.println("loginTest is a Pass");
-		}else{
-			System.out.println("loginTest is a Fail");
-		}*/
-		
-		System.out.println(driver.getTitle());
-		
-		driver.switchTo().frame(0);
-		
+		webdriver.executeScript("arguments[0].click();", submit);
+		driver.switchTo().frame(1);
 		int size = driver.findElements(By.name("iframe")).size();
 		System.out.println("iframe size is " + size);
-		
-		//driver.findElement(By.xpath("//a[@href='https://www.freecrm.com/index.cfm?logout=1']")).click();
-		
-		Thread.sleep(3000);
-		webdriver.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[@href='https://www.freecrm.com/index.cfm?logout=1']")));
-		
+		logout = driver.findElement(By.xpath("//a[@href='https://www.freecrm.com/index.cfm?logout=1']"));
+		WaitMethods.clickMethod(driver, logout, 3);
 	}
 	
 	@Test(dependsOnMethods =  {"loginTest"})
